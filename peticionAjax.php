4bd6c9c2a44a66bf62674526d35b3eb09f99f387
CarlosJ01 <61306700+CarlosJ01@users.noreply.github.com>
2020-03-12 23:23:44 -0600

    Archivo para la petición AJAX
    El documento junto con los demás archivos deben de estar en la carpeta de www o htdocs

---
+++ peticionAjax.php
@@ -0,0 +1,10 @@
+<?php
+    $json = array();
+
+    $json['HTML'] = 'HyperText Markup Language';
+    $json['CSS'] = 'Cascading Style Sheets';
+    $json['JS'] = 'JavaScript';
+    $json['PHP'] = 'Hypertext Preprocessor';
+
+    print_r(json_encode($json));
+?>
